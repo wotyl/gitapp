f16dda3197265a9b235fb404512fef74d29b3794
wotyl <wotyl63@gmail.com>
2016-04-30 23:03:23 +0300

    выв
    выв

---
+++ worke3.js
@@ -0,0 +1,21 @@
+$('body').on('click', '.serif', function(){
+	var arr_search = ['рус', 'доб'];
+	for(var i=0; i<arr_search.length; i++){
+		$('input_back_wrap').css('display','none');
+		$('#v_search').val(arr_search[i]);
+		var e = $.Event("keydown", { keyCode: 13 }); 
+		$('#v_search').trigger(e);
+
+	    setTimeout(function(){
+		    $('.resultif').append("<p>"+$('#video_local_search_summary').html()+"</p>");
+		    for(var i=0; i<$('#video_rows>div').length; i++){
+		    	$('.resultif').append("<p>"+$('#video_rows>div>div')[i].children[0].children[0].href+"</p>");
+		    	$('.resultif').append("<p>"+$.trim($('#video_rows>div>div')[i].children[1].children[0].children[0].innerHTML).replace("<em>","").replace("</em>","")+"</p>");
+		    	
+		    }
+        }, 1000);
+		
+
+	}
+})
+//https://raw.githubusercontent.com/wotyl/gitapp/master/worke0.js
